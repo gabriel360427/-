828e79e1ade323444cbc023812644a39ddaf4e5d
DINGtrue321 <zwz1324973312@163.com>
2024-06-18 17:57:10 +0800

    没有重分类的但是有分类统计

---
+++ DXApplication1/Custom Forms/classificationStatisticForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 城市空间生态格局智能评估系统.Custom_Forms
+{
+    public partial class classificationStatisticForm : DevExpress.XtraEditors.XtraForm
+    {
+        public classificationStatisticForm()
+        {
+            InitializeComponent();
+        }
+
+        private string filePath;
+
+        private void inputImageBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "栅格数据(*.img)|*.tif;*.tiff;*.img;*.dat";
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            filePath = openFileDialog.FileName;
+            inputImageTextBox.Text = filePath;
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            #region 1、参数设置
+            PIE.CommonAlgo.StclassStat info = new PIE.CommonAlgo.StclassStat();
+            info.inputname = filePath;
+            PIE.SystemAlgo.ISystemAlgo algo = PIE.SystemAlgo.AlgoFactory.Instance().CreateAlgo("PIE.CommonAlgo.dll", "PIE.CommonAlgo.ImgClassPostStaAlgo");
+            if (algo == null) return;
+            #endregion
+            //2、算法执行
+            PIE.SystemAlgo.ISystemAlgoEvents algoEvents = algo as PIE.SystemAlgo.ISystemAlgoEvents;
+            algo.Name = " 分类统计";
+            algo.Params = info;
+            bool result = PIE.SystemAlgo.AlgoFactory.Instance().ExecuteAlgo(algo);
+            info = algo.Params as PIE.CommonAlgo.StclassStat;
+            classificationStatisticTextBox.Text = info.statinfo;
+        }
+
+        private void exportBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "文本文件(*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName, true);
+                streamWriter.WriteLine(this.classificationStatisticTextBox.Text);
+                streamWriter.Close();
+                MessageBox.Show("导出成功！");
+            }
+        }
+    }
+}
---
+++ DXApplication1/Custom Forms/classificationStatisticForm.designer.cs
@@ -0,0 +1,154 @@
+﻿namespace 城市空间生态格局智能评估系统.Custom_Forms
+{
+    partial class classificationStatisticForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(classificationStatisticForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputImageTextBox = new System.Windows.Forms.TextBox();
+            this.inputImageBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.classificationStatisticTextBox = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(10, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入文件：";
+            // 
+            // inputImageTextBox
+            // 
+            this.inputImageTextBox.Location = new System.Drawing.Point(131, 30);
+            this.inputImageTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.inputImageTextBox.Name = "inputImageTextBox";
+            this.inputImageTextBox.Size = new System.Drawing.Size(627, 26);
+            this.inputImageTextBox.TabIndex = 1;
+            // 
+            // inputImageBtn
+            // 
+            this.inputImageBtn.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.inputImageBtn.Location = new System.Drawing.Point(763, 22);
+            this.inputImageBtn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.inputImageBtn.Name = "inputImageBtn";
+            this.inputImageBtn.Size = new System.Drawing.Size(119, 37);
+            this.inputImageBtn.TabIndex = 2;
+            this.inputImageBtn.Text = "浏览";
+            this.inputImageBtn.UseVisualStyleBackColor = true;
+            this.inputImageBtn.Click += new System.EventHandler(this.inputImageBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.classificationStatisticTextBox);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 76);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Size = new System.Drawing.Size(870, 459);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "分类统计结果";
+            // 
+            // classificationStatisticTextBox
+            // 
+            this.classificationStatisticTextBox.Location = new System.Drawing.Point(7, 35);
+            this.classificationStatisticTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.classificationStatisticTextBox.Multiline = true;
+            this.classificationStatisticTextBox.Name = "classificationStatisticTextBox";
+            this.classificationStatisticTextBox.Size = new System.Drawing.Size(859, 408);
+            this.classificationStatisticTextBox.TabIndex = 0;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.StartBtn.Location = new System.Drawing.Point(97, 550);
+            this.StartBtn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(210, 54);
+            this.StartBtn.TabIndex = 4;
+            this.StartBtn.Text = "开始统计";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.exportBtn.Location = new System.Drawing.Point(579, 550);
+            this.exportBtn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(210, 54);
+            this.exportBtn.TabIndex = 5;
+            this.exportBtn.Text = "导出文件";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.exportBtn_Click);
+            // 
+            // classificationStatisticForm
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 630);
+            this.Controls.Add(this.exportBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.inputImageBtn);
+            this.Controls.Add(this.inputImageTextBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "classificationStatisticForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "分类统计";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputImageTextBox;
+        private System.Windows.Forms.Button inputImageBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox classificationStatisticTextBox;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button exportBtn;
+    }
+}
