d9105664f82187f9870c67e4943f2390a55694d1
DINGtrue321 <zwz1324973312@163.com>
2024-06-20 00:49:17 +0800

    6.20 修改1

--- DXApplication1/Custom Tools/SpatialQueryTool.cs
+++ DXApplication1/Custom Tools/SpatialQueryTool.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using PIE.Carto;
 using PIE.Controls;
 using PIE.AxControls;
@@ -42,16 +43,24 @@ namespace 城市空间生态格局智能评估系统.Custom_Tools
             envelope = trackExtent;
             queryFilter.SetSpatialQuery(envelope as IGeometry);
             IFeatureCursor featureCursor = featureLayer.FeatureClass.Search(queryFilter);
-            IFeature feature = null;
-            feature = featureCursor.NextFeature();
-            //地图显示得到的结果
-            map.ClearSelectionFeatures();
-            while (feature != null)
+            if (featureCursor != null)
             {
-                m_MapControl.FocusMap.SelectFeature(featureLayer as ILayer, feature);
+                IFeature feature = null;
                 feature = featureCursor.NextFeature();
+                //地图显示得到的结果
+                map.ClearSelectionFeatures();
+                while (feature != null)
+                {
+                    m_MapControl.FocusMap.SelectFeature(featureLayer as ILayer, feature);
+                    feature = featureCursor.NextFeature();
+                }
+                m_MapControl.PartialRefresh(ViewDrawPhaseType.ViewAll);
             }
-            m_MapControl.PartialRefresh(ViewDrawPhaseType.ViewAll);
+            else 
+            {
+                MessageBox.Show("所选图层不是矢量数据，请选择矢量数据！");
+            }
+            
         }
     }
 }
